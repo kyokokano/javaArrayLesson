d55eca3279872c0200366a2cc771bfe7c6809ce9
kyoko <edgwcnn4869@gmail.com>
2020-11-29 12:34:54 +0900

    回答

--- Q01/Main.java
+++ Q01/Main.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class Main{
 	public static void main(String[] args){
+		int[] arr = new int[5];
 		//要素数5のint配列作成(配列変数名はarr)
+		arr[2] = 10;
 		//index2に10を代入
 		//出力
 		System.out.println(Arrays.toString(arr));
--- Q06/Main.java
+++ Q06/Main.java
@@ -4,8 +4,14 @@ public class Main{
 		System.out.print("単語を入れてね>");
 		String word=new Scanner(System.in).next();
 		//文字数をもとにchar配列を作成(配列変数名chars)
+		char[] chars = new char[word.length()];
+		char[] c = new char[word.length()];
 		for(int i=0;i<chars.length;i++){
 			//配列にお尻の文字から詰めていく処理
+			c = word.toCharArray();
+
+			chars[i] = c[word.length()-i-1];
+
 		}
 		System.out.println(Arrays.toString(chars));
 	}
